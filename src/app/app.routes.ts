3731abfae93cc620ca4ab55dad74c5308395309c
Alexander Hilding <hildingx@gmail.com>
2024-04-16 12:03:52 +0200

    Startat upp angularprojekt och skapat komponenter, routat dessa och skapat grundläggande layout. Infogat bild.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ConverterComponent } from './pages/converter/converter.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'converter', component: ConverterComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '404', component: NotFoundComponent} ,
+    { path: '**', component: NotFoundComponent }
+];
