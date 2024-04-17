07520d5e90c0d236d659e1a28de412b664dbb4de
Alexander Hilding <hildingx@gmail.com>
2024-04-17 09:50:53 +0200

    Uppdaterat funktionaliteten för konverteringen.

--- src/app/pages/converter/converter.component.ts
+++ src/app/pages/converter/converter.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './converter.component.html',
   styleUrl: './converter.component.scss'
 })
@@ -12,31 +13,45 @@ export class ConverterComponent {
   //Egenskaper
   meter: number | null = null;
   feet: number | null = null;
+  meterResult: number | null = null;
+  feetResult: number | null = null;
   celsius: number | null = null;
   fahrenheit: number | null = null;
+  celResult: number | null = null;
+  fahResult: number | null = null;
 
   //Metoder
   convertToFeet(): void {
+    //Om meter inte är null
     if(this.meter != null) {
-      this.feet = Number((this.meter * 3.28084).toFixed(2));
+      //Beräkna fot och avrunda till två decimaler
+      this.feetResult = Number((this.meter * 3.28084).toFixed(2));
+    } else {
+      this.feetResult = null;
     }
   }
 
   convertToMeter(): void {
     if(this.feet != null) {
-      this.meter = Number((this.feet * 0.3048).toFixed(2));
+      this.meterResult = Number((this.feet * 0.3048).toFixed(2));
+    } else {
+      this.meterResult = null;
     }
   }
   
   convertToFahrenheit(): void {
     if(this.celsius != null) {
-      this.fahrenheit = Number(((this.celsius * 9/5) + 32).toFixed(2));
+      this.fahResult = Number(((this.celsius * 9/5) + 32).toFixed(2));
+    } else {
+      this.fahResult = null;
     }
   }
 
   convertToCelsius(): void {
     if(this.fahrenheit != null) {
-      this.celsius = Number(((this.fahrenheit - 32) * 5/9).toFixed(2));
+      this.celResult = Number(((this.fahrenheit - 32) * 5/9).toFixed(2));
+    } else {
+      this.celResult = null;
     }
   }
 }
