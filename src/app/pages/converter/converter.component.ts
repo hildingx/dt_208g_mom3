069878ba4163ac69b9a031ad574cd145df81fe0e
Alexander Hilding <hildingx@gmail.com>
2024-04-17 09:25:00 +0200

    Gjort en konverterings-funktion i converter komponenten. Stylat detta och även stylat not-found komponenten.

--- src/app/pages/converter/converter.component.ts
+++ src/app/pages/converter/converter.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './converter.component.html',
   styleUrl: './converter.component.scss'
 })
 export class ConverterComponent {
+  //Egenskaper
+  meter: number | null = null;
+  feet: number | null = null;
+  celsius: number | null = null;
+  fahrenheit: number | null = null;
 
+  //Metoder
+  convertToFeet(): void {
+    if(this.meter != null) {
+      this.feet = Number((this.meter * 3.28084).toFixed(2));
+    }
+  }
+
+  convertToMeter(): void {
+    if(this.feet != null) {
+      this.meter = Number((this.feet * 0.3048).toFixed(2));
+    }
+  }
+  
+  convertToFahrenheit(): void {
+    if(this.celsius != null) {
+      this.fahrenheit = Number(((this.celsius * 9/5) + 32).toFixed(2));
+    }
+  }
+
+  convertToCelsius(): void {
+    if(this.fahrenheit != null) {
+      this.celsius = Number(((this.fahrenheit - 32) * 5/9).toFixed(2));
+    }
+  }
 }
